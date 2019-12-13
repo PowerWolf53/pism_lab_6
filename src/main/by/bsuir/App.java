3d48993b5e84a293d54d60f164ad9d0b8bb37169
Maksim_Baravy <Maksim_Baravy@epam.com>
2019-12-13 15:15:21 +0300

    first commit

---
+++ src/main/by/bsuir/App.java
@@ -0,0 +1,13 @@
+package by.bsuir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/by/bsuir/decorator/ListDecorator.java
@@ -0,0 +1,12 @@
+package by.bsuir.decorator;
+
+import java.util.ArrayList;
+
+public class ListDecorator {
+
+    private ArrayList<String> list = new ArrayList<>();
+
+    public ArrayList<String> getList(){
+        return this.list;
+    }
+}
